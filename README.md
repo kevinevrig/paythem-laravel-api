84a89639f5a3d1801e90e27db02f29dd3a47ca65
kevinevrig <kevin@evrig.com>
2022-09-07 12:18:22 +0530

    Update
    Add following endpoints
    - Buy Vouchers
    - Product is available or not
    - Get all available products
    - Get last sale
    - Get product formats
    - Get sale transactions
    - Get financial transactions

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# Paythem
- Paythem API v2 Laravel Package
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/kevinevrig/paythem-laravel-api?style=for-the-badge)](https://packagist.org/packages/kevinevrig/paythem-laravel-api)
+[![License](https://img.shields.io/github/license/kevinevrig/paythem-laravel-api?style=for-the-badge)](https://tldrlegal.com/license/mit-license)
+![PHP](https://img.shields.io/packagist/php-v/kevinevrig/paythem-laravel-api?style=for-the-badge)
+[![Total Downloads](https://img.shields.io/packagist/dt/kevinevrig/paythem-laravel-api?style=for-the-badge)](https://packagist.org/packages/kevinevrig/paythem-laravel-api)
+
+# Paythem API v2 - Laravel Package
+
+Simple Laravel Package for Paythem API v2
 
 ## Installation
 
 Install the package by the following command,
 
-    composer require paythem/ptn
-
-## Add Provider
-
-Add the provider to your `config/app.php` into `provider` section if using lower version of laravel,
-
-    Paythem\Ptn\PaythemServiceProvider::class,
+    composer require kevinevrig/ptn
 
 ### License
 The MIT License (MIT). Please see [License](LICENSE.md) File for more information   
--- src/PaythemApi.php
+++ src/PaythemApi.php
@@ -10,8 +10,10 @@ class PaythemApi extends Controller
     private object $paythem;
     private string $env;
     private int $appID;
-
     private string $endpoint;
+
+    public array $apiParameters;
+    public string $reference_id;
 
     public function __construct( string $environment = '', int $appID = 2848, string $iv = '', array $user_credentials = [] )
     {
@@ -24,67 +26,23 @@ class PaythemApi extends Controller
         $this->paythem->USERNAME    = $user_credentials['USERNAME'];
         $this->paythem->PASSWORD    = $user_credentials['PASSWORD'];
         $this->paythem->SERVER_URI  = 'https://vvs'.$this->env.'.paythem.net/API';
+
+        $this->apiParameters = array();
+        $this->reference_id = '';
     }
 
     /**
-     * Get Account Balance from Paythem
-     *
-     * @param bool $json
-     * @param bool $debug
-     * @return null
-     */
-    public function getAccountBalance( bool $json = false, bool $debug = false )
-    {
-        return $this->apiCall( 'get_AccountBalance', $json, $debug );
-    }
-
-    /**
-     * Get Product List from Paythem
-     *
-     * @param bool $json
-     * @param bool $debug
-     * @return null
-     */
-    public function getProductList( bool $json = false, bool $debug = false )
-    {
-        return $this->apiCall( 'get_ProductList', $json, $debug );
-    }
-
-    /**
-     * Get Brand List from Paythem
-     *
-     * @param bool $json
-     * @param bool $debug
-     * @return null
-     */
-    public function getBrandList( bool $json = false, bool $debug = false )
-    {
-        return $this->apiCall( 'get_BrandList', $json, $debug );
-    }
-
-    /**
-     * Get OEM List from Paythem
-     *
-     * @param bool $json
-     * @param bool $debug
-     * @return null
-     */
-    public function getOEMList( bool $json = false, bool $debug = false )
-    {
-        return $this->apiCall( 'get_OEMList', $json, $debug );
-    }
-
-    /**
-     * To make API Calls
+     * Make API Calls
      *
      * @param string $endpoint
      * @param bool $json
      * @param bool $debug
-     * @return void
+     * @return mixed
      */
-    private function apiCall( string $endpoint, bool $json = false, bool $debug = false )
+    private function apiCall( string $endpoint, bool $json = false, bool $debug = false ): mixed
     {
         $this->paythem->FUNCTION = $endpoint;
+        $this->paythem->PARAMETERS = $this->apiParameters;
 
         $response = $this->paythem->callAPI( $debug );
 
@@ -93,5 +51,175 @@ class PaythemApi extends Controller
         else:
             return json_encode($response);
         endif;
+    }
+
+    /**
+     * Get a full list of OEMs in the VVS system.
+     *
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getOEMList( bool $json = false, bool $debug = false ): mixed
+    {
+        return $this->apiCall( 'get_OEMList', $json, $debug );
+    }
+
+    /**
+     * Get a full list of brands by OEMs in the VVS system.
+     *
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getBrandList( bool $json = false, bool $debug = false ): mixed
+    {
+        return $this->apiCall( 'get_BrandList', $json, $debug );
+    }
+
+    /**
+     * Get a full list of active products in the VVS system by brand by OEM.
+     * Recommended to perform once a day.
+     *
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getProductList( bool $json = false, bool $debug = false ): mixed
+    {
+        return $this->apiCall( 'get_ProductList', $json, $debug );
+    }
+
+    /**
+     * Get the company’s current account balance.
+     * Balance is returned on each sales transaction.
+     * Recommended to perform every 15 minutes.
+     *
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getAccountBalance( bool $json = false, bool $debug = false ): mixed
+    {
+        return $this->apiCall( 'get_AccountBalance', $json, $debug );
+    }
+
+    /**
+     * Get a specific product’s availability.
+     * Recommended to perform before each sale with selling product’s ID.
+     *
+     * @param int $product_id
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function isAvailable( int $product_id, bool $json = false, bool $debug = false ): mixed
+    {
+        $this->apiParameters = array(
+            "PRODUCT_ID" => $product_id
+        );
+        return $this->apiCall( 'get_ProductAvailability', $json, $debug );
+    }
+
+    /**
+     * Get all products’ availability.
+     * Recommended to perform every 15 minutes.
+     *
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getAllAvailableProducts( bool $json = false, bool $debug = false ): mixed
+    {
+        return $this->apiCall( 'get_AllProductAvailability', $json, $debug );
+    }
+
+    /**
+     * Get a single or multiple vouchers from VVS.
+     * It generates a transaction on VVS which can later be queried.
+     *
+     * @param int $product_id
+     * @param int $quantity
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function buyVouchers( int $product_id, int $quantity, bool $json = false, bool $debug = false ): mixed
+    {
+        $this->apiParameters = array(
+            'PRODUCT_ID'    => $product_id,
+            'QUANTITY'      => $quantity,
+            'REFERENCE_ID'  => $this->reference_id
+        );
+
+        return $this->apiCall( 'get_Vouchers', $json, $debug );
+    }
+
+    /**
+     * Get all vouchers purchased on the last sale
+     *
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getLastSale( bool $json = false, bool $debug = false ): mixed
+    {
+        return $this->apiCall( 'get_LastSale', $json, $debug );
+    }
+
+    /**
+     * Get all vouchers purchased during a specific period for API user.
+     *
+     * TODO: SaleTransactions : 30 days calculation remaining
+     *
+     * @param string $from_date
+     * @param string $to_date
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getSaleTransactions( string $from_date, string $to_date, bool $json = false, bool $debug = false ): mixed
+    {
+        $this->apiParameters = array(
+            'FROM_DATE' => date("Y-m-d", strtotime($from_date)),
+            'TO_DATE'   => date("Y-m-d", strtotime($to_date))
+        );
+
+        return $this->apiCall( 'get_SalesTransaction_ByDateRange', $json, $debug );
+    }
+
+    /**
+     * Get all financial transactions (deposits, credits, reversals, purchases, etc) for company.
+     *
+     * TODO: FinancialTransaction : 30 days calculation remaining
+     *
+     * @param string $from_date
+     * @param string $to_date
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getFinancialTransaction( string $from_date, string $to_date, bool $json = false, bool $debug = false ): mixed
+    {
+        $this->apiParameters = array(
+            'FROM_DATE' => date("Y-m-d", strtotime($from_date)),
+            'TO_DATE'   => date("Y-m-d", strtotime($to_date))
+        );
+
+        return $this->apiCall( 'get_FinancialTransaction_ByDateRange', $json, $debug );
+    }
+
+    /**
+     * Retrieves all voucher formats for products.
+     *
+     * NOTE: This function is not available in development phase.
+     *
+     * @param bool $json
+     * @param bool $debug
+     * @return mixed
+     */
+    public function getProductFormats( bool $json = false, bool $debug = false ): mixed
+    {
+        return $this->apiCall( 'get_ProductFormats', $json, $debug );
     }
 }
